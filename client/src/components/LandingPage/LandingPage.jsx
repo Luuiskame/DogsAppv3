02adb7996f3838e52c58a1914759282eb9e4a831
Luis Reyes <yuiftw05@gmail.com>
2023-10-29 01:24:05 -0600

    starting landing page styles

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from './LandingPage.module.css'
+const apiDoc = "https://www.thedogapi.com/"
 
 
 function LandingPage(){
@@ -8,24 +9,32 @@ function LandingPage(){
         <nav className={styles.landingNav}>
         <p className={styles.logo}><span className={styles.spanLogo}>Happ</span>Dog</p>
         <Link to='/home'>
-        <button className={styles.takeHomeBtn}><span>Home</span></button>
+        <button className={styles.takeHomeBtn}><i className="material-icons">home</i></button>
         </Link>
         </nav>
         <div className={styles.imgAndTextContainer}>
-            <div className={styles.imgContainer}>
-            <img />
-            </div>
-            
-            <div className={styles.textContainer}>
-            <h1><span className={styles.honeSpan}>Get</span> started</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur consectetur iusto dicta facere quos quo praesentium quibusdam quidem deleniti maxime impedit nulla.</p>
+       
+        <h1 className={styles.h1Landing}> All dogs data you need directly from <span className={styles.h1Gradient}>thedogApi</span></h1>
 
-        </div>
+        <button className={styles.takeApiBtn}><a href={apiDoc} target="_blank" rel="noopener noreferrer">Learn more</a></button>
+            <div className={styles.imgContainer}>
+            <img src="/Img/undraw_good_doggy_re_eet7.svg"/>
+            </div>
+
         </div>
 
         <div className={styles.aboutContainer}>
-            <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur consectetur iusto dicta facere quos quo praesentium quibusdam quidem deleniti maxime impedit nulla.</p>
+            <h2 className={styles.landingNumbers}>1</h2>
+            <h3 className={styles.numberTitles}>Search your favorites dogs by breed</h3>
+            <p className={styles.landingP}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur consectetur iusto dicta facere quos quo praesentium quibusdam quidem deleniti maxime impedit nulla.</p>
+
+            <h2 className={styles.landingNumbers}>2</h2>
+            <h3 className={styles.numberTitles}>Filter them by all options we provide</h3>
+            <p className={styles.landingP}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur consectetur iusto dicta facere quos quo praesentium quibusdam quidem deleniti maxime impedit nulla.</p>
+
+            <h2 className={styles.landingNumbers}>3</h2>
+            <h3 className={styles.numberTitles}>Create your own dog using AI</h3>
+            <p className={styles.landingP}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur consectetur iusto dicta facere quos quo praesentium quibusdam quidem deleniti maxime impedit nulla.</p>
 
         </div>
         
