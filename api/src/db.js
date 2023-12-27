cb9725f53100fb2cfeb4a835ffea4ad6a20194ad
Luis Reyes <yuiftw05@gmail.com>
2023-12-27 10:33:51 -0600

    fix: trying dialect with postgres for deploying

--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  dialectModule: require('pg'),
+  dialect: 'postgres',
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
