a72a2ba308a28be297c1a010d5536075433b2d57
Luis Reyes <yuiftw05@gmail.com>
2024-01-10 13:49:48 -0600

    db.js: requiring pg and using it as a dialect mode for deployment

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const pg = require('pg')
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
@@ -7,7 +8,7 @@ const {
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  dialect: 'postgres',
+  dialectModule: pg,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
