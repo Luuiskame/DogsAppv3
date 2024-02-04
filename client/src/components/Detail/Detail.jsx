964dacf00a5747a655539f5be42c20ba3d3cb1d5
Luis Reyes <yuiftw05@gmail.com>
2024-02-04 15:03:41 -0600

    updating default image path

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -3,7 +3,7 @@ import {useParams} from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-const defaultImage = '../../../public/Img/dogDefault_img.png'
+const defaultImage = '/Img/dogDefault_img.png';
 
 //? styles
 import styles from './Detail.module.css'
