ef5ea67b51b82228e31523cec2271a7596e662f3
Luis Reyes <yuiftw05@gmail.com>
2024-09-21 07:34:42 -0600

    fix: using new cors url to allow petition

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors'); // Import cors middleware
 const routes = require('./routes/index.js');
 
 require('./db.js');
@@ -10,22 +11,23 @@ const server = express();
 
 server.name = 'API';
 
+// Configure CORS
+const corsOptions = {
+  origin: ['https://dogs-appv3.vercel.app', 'https://dogs-appv3-r3yj.vercel.app'], // Specific origins
+  credentials: true, // Allow cookies and credentials
+};
+
+server.use(cors(corsOptions)); // Use cors with specified options
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',  "*"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
+// Routes
 server.use('/dogsapp', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+// Error catching middleware
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
